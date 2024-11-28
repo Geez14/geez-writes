e7058547a370ffa7fb1d3ba22422284f83814ae3
Monabbar Anjum <91541128+Geez14@users.noreply.github.com>
2024-11-28 20:45:17 +0530

    Add feature, add blogs (#4)
    we can now add blogs in page
    
    Co-authored-by: Md Monabber Anjum <monabbar@noreply.com>

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 # from . import views
-from .views import HomeView, DetailedPostView
+from .views import HomeView, DetailedPostView, AddPostView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("blog/<int:pk>", DetailedPostView.as_view(), name="blog-detail"),
+    path("add-post/", AddPostView.as_view(), name="add-post"),
 ]
