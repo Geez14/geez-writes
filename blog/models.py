57e8d0e13fb0f6561b9cc9ff255ff64192221a59
Monabbar Anjum <91541128+Geez14@users.noreply.github.com>
2024-11-28 18:47:19 +0530

    Revert "Feature/breaking change/modified url pattern"

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-from django.contrib.auth.models import User
-
-
-class BlogPost(models.Model):
-    title = models.CharField(max_length=342)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
-    
-    def __str__(self):
-        return str(self.title) + " | " + str(self.author)
+# Create your models here.
