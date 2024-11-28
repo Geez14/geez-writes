d028045f72e2d18f94ece6c949f8e4790b4b4b79
Md Monabber Anjum <monabbar@noreply.com>
2024-11-29 02:45:25 +0530

    Feat: add django form for adding post
    integrate bootstrap in the add_post.html
    
    fix-minor-bugs

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-
 class BlogPost(models.Model):
     title = models.CharField(max_length=342)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import *
+from .forms import PostForm
+
 
 class HomeView(ListView):
     model = BlogPost
@@ -12,7 +14,8 @@ class DetailedPostView(DetailView):
 
 class AddPostView(CreateView):
     model = BlogPost
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+    # fields = '__all__' # we have to remove this because of form_class!
     # we can use cherry pick too as many of the items can be null
     # fields = ('title', 'body')
