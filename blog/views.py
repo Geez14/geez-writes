99057a5be070b8a725397d38a316fb7b853c8f9e
Monabbar Anjum <91541128+Geez14@users.noreply.github.com>
2024-11-29 03:03:03 +0530

    Feature/custom form (#5)
    * Add feature, add blogs
    
    we can now add blogs in page
    
    * Feat: add django form for adding post
    
    integrate bootstrap in the add_post.html
    
    fix-minor-bugs
    
    ---------
    
    Co-authored-by: Md Monabber Anjum <monabbar@noreply.com>

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
-from django.views.generic import ListView, DetailView, CreateView
 from .models import *
+from .forms import PostForm
+
 from .forms import PostForm
 
 
@@ -12,6 +13,14 @@ class HomeView(ListView):
 class DetailedPostView(DetailView):
     model = BlogPost
     template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = BlogPost
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = '__all__' # we have to remove this because of form_class!
+    # we can use cherry pick too as many of the items can be null
+    # fields = ('title', 'body')
 
 class AddPostView(CreateView):
     model = BlogPost
